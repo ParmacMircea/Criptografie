d9444f8b9d8831228bd21d3ff56d4bd62a443aa1
ParmacMircea <mircea.parmac@gmail.com>
2025-03-15 22:20:35 +0200

    Add files via upload

---
+++ Tema 3/Jacobi/Jacobi.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int simbol_Jacobi(int a, int n) {
+    if (n <= 0 || n % 2 == 0) return 0; // n trebuie sa fie impar și pozitiv
+    int rezultat = 1;
+    a = a % n;
+
+    while (a != 0) {
+        while (a % 2 == 0) {
+            a = a / 2;
+            if (n % 8 == 3 || n % 8 == 5)
+                rezultat = -rezultat;
+        }
+
+        // aplicam reciprocitatea patratica
+        int temp = a;
+        a = n;
+        n = temp;
+
+        if (a % 4 == 3 && n % 4 == 3)
+            rezultat = -rezultat;
+
+        a = a % n;
+    }
+
+    if (n == 1)
+        return rezultat;
+    else
+        return 0;
+}
+
+int main() {
+    int a, n;
+    cout << "Calcul simbol Jacobi (a/n)\n";
+    cout << "Introdu a: ";
+    cin >> a;
+    cout << "Introdu n (impar si pozitiv): ";
+    cin >> n;
+
+    int r = simbol_Jacobi(a, n);
+    cout << "Simbolul Jacobi (" << a << "/" << n << ") = " << r << endl;
+
+    return 0;
+}
---
+++ Tema 3/Solovay – Strassen/S-S.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+int powmod(int a, int b, int n) {
+    int result = 1;
+    a = a % n;
+    while (b > 0) {
+        if (b % 2 == 1)
+            result = (result * a) % n;
+        a = (a * a) % n;
+        b = b / 2;
+    }
+    return result;
+}
+
+int Simbol_Jacobi(int a, int n) {
+    if (n <= 0 || n % 2 == 0) return 0;
+    int r = 1;
+    a = a % n;
+    while (a != 0) {
+        while (a % 2 == 0) {
+            a = a / 2;
+            if (n % 8 == 3 || n % 8 == 5)
+                r = -r;
+        }
+        int temp = a;
+        a = n;
+        n = temp;
+        if (a % 4 == 3 && n % 4 == 3)
+            r = -r;
+        a = a % n;
+    }
+    if (n == 1)
+        return r;
+    else
+        return 0;
+}
+
+int TEST_Solovay_Strassen(int n, int attempts) {
+    if (n == 2) return 1;
+    if (n < 2 || n % 2 == 0) return 0;
+
+    for (int i = 0; i < attempts; i++) {
+        int b = 2 + (i * 3) % (n - 2);
+        int j = Simbol_Jacobi(b, n);
+        if (j == 0 || powmod(b, (n - 1) / 2, n) != (j + n) % n)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n, k;
+    cout << "Test Solovay-Strassen (primalitate)\n";
+    cout << "Introdu numarul n: ";
+    cin >> n;
+    cout << "Numar de incercari (martori): ";
+    cin >> k;
+
+    if (TEST_Solovay_Strassen(n, k) == 1)
+        cout << "Numarul " << n << " este probabil prim.\n";
+    else
+        cout << "Numarul " << n << " este compus.\n";
+
+    return 0;
+}
